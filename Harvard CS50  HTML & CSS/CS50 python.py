6e3cb9510d7da6e13384792ce4ecc6d3e5d1af72
Julius170 <juliusayoola5@gmail.com>
2021-10-19 20:55:47 +0100

    Condition Added

--- Harvard CS50  HTML & CSS/CS50 python.py
+++ Harvard CS50  HTML & CSS/CS50 python.py
@@ -1,2 +1,26 @@
-print ('Hello World')
+# BASIC PRINT
+
+print ("Hello World");
 print ("My name is Phensic")
+Name = input("Name: ")
+print(f"Hello, {Name}")
+
+# CONDITIONS
+
+num = int(input('Number: '))
+if num > 0:
+    print("This is a positive number")
+else:
+    print("Number is not positive")
+
+
+    
+
+pref = int(input('Number: '))
+if pref > 11:
+    print('This number is greater than 11')
+elif pref < 11 :
+    print('This number is less than 11')
+else: 
+    print('The number is 11 ')
+    
