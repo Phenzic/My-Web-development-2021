a6a9f3933dbd728c2dd43c149582a0042a43cf63
Phensic <juliusayoola5@gmail.com>
2021-10-25 16:00:36 +0100

    Added sequence, functions, loops and OOP

--- Harvard CS50  HTML & CSS/CS50 python.py
+++ Harvard CS50  HTML & CSS/CS50 python.py
@@ -24,3 +24,85 @@ elif pref < 11 :
 else: 
     print('The number is 11 ')
     
+#  SEQUENCE
+# LISTS => A SEQUENCE OF MUTABLE VALUES 
+name = "Mayor"
+print(name[1])
+
+
+
+# TUPLE =>  SEQUENCE OF IMMUTABLE VALUES
+coordinate = (10.0, 20.0)
+
+Name = ["Mayor", "Henry", "Ron", " Hermoine", "Joycee",]
+Name.append("Draco")
+print(Name[0])
+Name.sort()
+print(Name)
+
+
+
+# SET => A COLLECTION OF UNIWUE VALUES
+
+s = set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(2) # ONLY RETURNS UNIQUE VALUES 
+
+s.remove(3)
+print(s)
+print(f"The set has {len(s)} elements.")
+
+# DICTIONARY => A COLLECTION OF KEY-VALUE PAIR
+
+houses ={"Harry": "Gryffindor", "Draco": "Slytherin"}
+houses["Hermoine"] = "Gryffindor"
+print(houses["Hermoine"])
+
+
+
+
+# LOOPS
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+for i in range(20):
+    print(i)
+
+Names = ["Rayn", "Raynold", "Blue Shirt Guy", "Tom", "Welling"]
+
+for inputs in Names:
+    print(inputs)
+
+person = "Salvatore"
+for chars in person:
+    print(chars)
+
+
+
+# FUNCTIONS
+
+def Square(x):
+    return(x * x)
+
+for i in range(10):
+    print(f"The square of {i} is {Square(i)}")
+
+# ON ANOTHER FILE YOU CAN IMPORT FUNCTIONS BY USING THE INBUILT FUNCTION:
+# FROM <FILE NAME> IMPORT SQUARE
+
+
+
+
+# PYTHON OBJECT-ORIENTED PROGRAMMING 
+
+# # CLASS
+# class Point():
+#     def __init__(self, x, y)
+
+    
+
+
