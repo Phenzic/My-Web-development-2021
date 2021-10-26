b95d94656e419d119a7d62642427107485e4b672
Phensic <juliusayoola5@gmail.com>
2021-10-26 03:31:33 +0100

    Concluded

--- Harvard CS50  HTML & CSS/CS50 python.py
+++ Harvard CS50  HTML & CSS/CS50 python.py
@@ -99,10 +99,103 @@ for i in range(10):
 
 # PYTHON OBJECT-ORIENTED PROGRAMMING 
 
-# # CLASS
-# class Point():
-#     def __init__(self, x, y)
+# CLASS
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+p = Point(2, 8)
+
+print(p.x)
+print(p.y)
     
 
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passangers = []
 
+    def add_passangers(self, name):
+        if not self.open_seats():
+            return False    
+        self.passangers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passangers) 
+
+flight = Flight(3)
+
+people = ["Harry", "Ron", "Hermoine", "Ginny"]
+for persons in people:
+    if flight.add_passangers(persons):
+        print(f"Added {persons} to the flight succcessfully")
+    else:
+        print(f"There are no available seats for {persons}")
+
+
+
+
+# DECORATOR => ARE MODIFYERS OF FUNCTIONS
+# ( TAKE A FUNCTION WRAPS IT UP AND PRINT OUT A MODIFIED VERSION OF IT)
+
+
+def announce(f):
+    def wrapper():
+        print("About to run a function...")
+        f()
+        print("Done with the function.")
+    return wrapper
+
+
+
+@announce         #ADDING THE ANNOUNCE DECORATOR
+def hello():
+    print("Hello, World!")
+
+hello()
+
+
+
+
+
+
+# LAMBDA 
+
+Groups = [
+    {"Name": "Harry", "House": "Gryffindor"},
+    {"Name": "Cho", "House": "Ravenclaw"},
+    {"Name": "Draco", "House": "Slytherin"}
+]
+
+# def f(person):
+#     return person["House"]
+# Groups.sort(key = f)
+#       OR
+
+Groups.sort(key = lambda person: person["Name"])
+
+print(Groups)
+
+
+# EXCEPTIONS
+import sys
+
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("Error: Invalid input.")
+    sys.exit(1)
+
+
+
+
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("Error: Cannot divide by 0. ")
+    sys.exit(1)
+
+print (f"{x} / {y} = {result}")
