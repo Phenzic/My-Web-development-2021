2877626ff72d2aec1a6b1e896ef66bc35407acc7
Phensic <juliusayoola5@gmail.com>
2021-11-09 00:50:21 +0100

    CS50 Django web dev

---
+++ Harvard CS50 Django/Web_dev/hello/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,"hello/index.html")
+
+def phensic(request):
+    return HttpResponse("Hello, Phensic!")
+
+def sunshine(request):
+    return HttpResponse("Hello, Sunshine!")
+
+def greet(request, name):
+    return render(request,"hello/greet.html", {
+         "name": name.capitalize()
+    })
+    # return HttpResponse(f"Hello, {name.capitalize()}!")
